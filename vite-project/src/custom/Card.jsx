9e63611f42574af174c887bd99c0b150fdc0ded5
sakib <sbsakibsarkar8@gmail.com>
2024-10-30 01:42:13 +0600

    card add

--- vite-project/src/custom/Card.jsx
+++ vite-project/src/custom/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './card.css'
-function Card() {
+function Card(props) {
   return (
-    <div>
+    <div className='mt-5'>
      <div className="card">
-  <img src="" className="card-img-top" alt="..."/>
+  <img src={props.img} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{props.text}</h5>
+    <p className="card-text">{props.ptext}</p>
+    <a href="#" className="btn btn-primary">{props.button}</a>
   </div>
   </div>
     </div>
--- vite-project/src/pages/Portfolio.jsx
+++ vite-project/src/pages/Portfolio.jsx
@@ -8,7 +8,25 @@ function Portfolio() {
        <h1 className={Sakib.nam}>sakib sarkar Portfolio</h1>
           
          <h1 className='bg-success'>sabbir sarkar </h1>
-       <Card/>
+
+       <div className='row'>
+        <div className='col-md-4'>
+        <Card img="img/bridge.png" text="Card title" ptext="card title and make up the bulk of the card's content." button="click me"/>
+       
+        </div>
+
+        <div className='col-md-4'>
+        <Card img="img/build.png" text="Card title" ptext="Some quick example text to build on the card title and make up the bulk of the card's content." button="click me"/>
+       
+        </div>
+
+        <div className='col-md-4'>
+        <Card img="img/unsplash 1.png" text="Card title" ptext="Some quick example text to build on the card title and make up the bulk of the card's content." button="click me"/>
+       
+        </div>
+
+       </div>
+
     </div>
   )
 }
