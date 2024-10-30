d6ded95e324dae7d1751f67dc95ef17fda9a2a4b
sakib <sbsakibsarkar8@gmail.com>
2024-10-30 17:25:27 +0600

    togol

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import Navbar from '../component/Navber';
 import Portfolio from './Portfolio';
 
 function Home() {
+  let [togol , settogol]= useState()
+  let togoler =()=>{
+    settogol(!togol)
+  }
     return (
       <div>
       <Navbar/>
         <h1>my name is home</h1>
-       
+        <button onClick={togoler}>
+          {togol ? "hide" : "show"}
+        </button>
+      {
+        togol &&  <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dignissimos id dolor!</p>
+      }
         <Portfolio/>
       </div>
     )
