2721ce2f17f235815f5541d5b57329022acf8459
sakib <sbsakibsarkar8@gmail.com>
2024-11-05 04:01:40 +0600

    new start to learn

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,9 @@
-
+import Navber from "./component/Navber";
 
 function App() {
   return (
     <div className="container">
-     
+     <Navber/>
      <h1 className=" bg-primary">hello sakib</h1>
     </div>
   )
