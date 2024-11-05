eff7aff64a72f0b05b21c49dc549599babfe6f71
sakib <sbsakibsarkar8@gmail.com>
2024-11-06 02:12:24 +0600

    navber done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import Navber from "./component/Navber";
 
 function App() {
   return (
-    <div className="container">
+    <div>
      <Navber/>
      <h1 className=" bg-primary">hello sakib</h1>
     </div>
--- vite-project/src/component/Navber.jsx
+++ vite-project/src/component/Navber.jsx
@@ -5,23 +5,23 @@ function Navber() {
     <>
    <nav class="navbar navbar-expand-lg navbar-light bg_color">
   <div class="container-fluid">
-    <a class="navbar-brand text-white" href="#">Navbar</a>
+    <a class="navbar-brand text-white" href="#">SB Sakib</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav text-white">
-        <li class="nav-item">
+      <ul class="navbar-nav text-white mx-auto">
+        <li class="nav-item mx-2 hove">
           <a class="nav-link text-white active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+        <li class="nav-item mx-2">
+          <a class="nav-link text-white" href="#">About</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+        <li class="nav-item mx-2">
+          <a class="nav-link text-white" href="#">service</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <li class="nav-item mx-2">
+          <a class="nav-link disabled text-white" href="#" tabindex="-1" aria-disabled="true">protfolio</a>
         </li>
       </ul>
     </div>
