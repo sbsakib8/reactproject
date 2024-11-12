4d48be58ad1918a282000a2f9189839ae10d9e4e
sakib <sbsakibsarkar8@gmail.com>
2024-11-12 16:57:56 +0600

    joy ui add

--- vite-project/src/pages/home/Home.jsx
+++ vite-project/src/pages/home/Home.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import * as React from 'react';
+import Table from '@mui/joy/Table';
 
-function Home() {
-  return (
-    <>
-    
-    </>
-  )
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
 }
 
-export default Home
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+ function Home() {
+  return (
+    <Table hoverRow>
+      <thead>
+        <tr>
+          <th style={{ width: '40%' }}>Column width (40%)</th>
+          <th>Calories</th>
+          <th>Fat&nbsp;(g)</th>
+          <th>Carbs&nbsp;(g)</th>
+          <th>Protein&nbsp;(g)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row) => (
+          <tr key={row.name}>
+            <td>{row.name}</td>
+            <td>{row.calories}</td>
+            <td>{row.fat}</td>
+            <td>{row.carbs}</td>
+            <td>{row.protein}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+export default Home;
