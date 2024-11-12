eff8523c6e5e6c3862445d97a568e5ff1fb05f22
sakib <sbsakibsarkar8@gmail.com>
2024-11-13 00:11:00 +0600

    matarial ui

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 
 import Form from "./component/form/Form.JSX";
 import Navber from "./component/Navber";
+import Navberui from "./component/Navberui";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div>
      <Navber/>
+     <Navberui />
      <Home />
      <Form/>
     </div>
